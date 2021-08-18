18f2b6accc438640e318c2bfd5f9ebea316a380d
sailaddu <85615147+sailaddu@users.noreply.github.com>
2021-08-18 12:35:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
+
 class App extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props);
+		this.state = { show: false };
 	};
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+	render() {
+		return (
+			<div id="main">
+				{ /* Do not remove this main div!! */}
+				<button id="click" onClick={() => {
+					this.setState({ show: true });
+				}} >Render Paragraph</button>
+				{this.state.show && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+				</div>
+		);
+	}
 }
 
 
